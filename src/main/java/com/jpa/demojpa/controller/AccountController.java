e75801e6fb2eb76bbefab8bc45f818fc3722c688
sreenath <sreenathbr20@gmail.com>
2024-03-05 18:02:30 +0530

    cross origin added to account controller

--- src/main/java/com/jpa/demojpa/controller/AccountController.java
+++ src/main/java/com/jpa/demojpa/controller/AccountController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:4200/")
+
 public class AccountController {
 
 	@Autowired
--- src/main/java/com/jpa/demojpa/entity/Account.java
+++ src/main/java/com/jpa/demojpa/entity/Account.java
@@ -1,11 +1,13 @@
 package com.jpa.demojpa.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Account {
     @Id
+    @GeneratedValue
     private Integer id;
 
     private String name;
